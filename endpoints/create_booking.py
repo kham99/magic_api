eef76c55a40973940265ab0d16c3d05798e758bc
Amir Khabiev <amir.khabiev@yandex.ru>
2024-08-28 16:08:50 +0300

    28.08.2024

--- endpoints/create_booking.py
+++ endpoints/create_booking.py
@@ -2,30 +2,26 @@ import requests
 from clients.http_client import HTTPClient
 
 
-class CreateBooking(HTTPClient):
+class Booking(HTTPClient):
 
     def __init__(self):
         self._http_client = HTTPClient()
 
-    def send_request_for_create_booking(self, firstname, lastname, general_totalprice, general_additionalneeds, checkin,
+    def _create_booking(self, firstname, lastname, general_totalprice, general_additionalneeds, checkin,
                                         checkout):
-        headers = {"Content-Type": "application/json", "Accept": "*/*"}
         body = {"firstname": firstname, "lastname": lastname, "totalprice": general_totalprice, "depositpaid": True,
                 "bookingdates": {"checkin": checkin, "checkout": checkout},
                 "additionalneeds": general_additionalneeds}
 
-
         response = self._send_request(url="https://restful-booker.herokuapp.com/booking",
                                        method="post",
-                                       headers=headers,
-                                       json=body
+                                       json=body,
                                        expected_status_code=200
                                        )
 
         self.status = response.status_code
         self.response = response.json()
         return response
-
 
     def check_booking_in_response_body(self):
         assert "booking" in self.response, "Тело ответа не содержит объект 'booking'"
--- tests/test_create_bookings.py
+++ tests/test_create_bookings.py
@@ -1,12 +1,12 @@
-from endpoints.create_booking import CreateBooking
+from endpoints.create_booking import Booking
 import pytest
 
 
 @pytest.mark.booking
 def test_create_booking(generate_firstname, generate_lastname, generate_totalprice, generate_additionalneeds, checkin,
                         checkout):
-    endpoint = CreateBooking()
-    endpoint.send_request_for_create_booking(generate_firstname, generate_lastname,
+    endpoint = Booking()
+    endpoint._create_booking(generate_firstname, generate_lastname,
                                              generate_totalprice, generate_additionalneeds, checkin, checkout,
                                              )
     endpoint.check_booking_in_response_body()
